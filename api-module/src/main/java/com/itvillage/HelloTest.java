98c13fdf3451b10cbc6cae184a57fcc5a5aae57c
hjs6877 <hjs6877@example.com>
2021-03-03 10:03:36 +0900

    .

---
+++ api-module/src/main/java/com/itvillage/HelloTest.java
@@ -0,0 +1,9 @@
+package com.itvillage;
+
+//import com.itvillage.utils.HelloUtil;
+
+public class HelloTest {
+    public static void main(String[] args) {
+//        System.out.println(HelloUtil.sayHello());
+    }
+}
